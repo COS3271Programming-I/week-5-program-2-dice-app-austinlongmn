c802d3e1bd19e4cecb10f53867af2c0685f54c64
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-09-30 18:48:50 -0500

    Wrote program

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Random;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      boolean rollAgain = true;
+
+      Random random = new Random();
+      while (rollAgain) {
+        int dice1 = random.nextInt(1, 7);
+        int dice2 = random.nextInt(1, 7);
+
+        System.out.format(
+          "You got %d and %d. That's %d all together.\n",
+          dice1,
+          dice2,
+          dice1 + dice2
+        );
+        System.out.println(
+          "Press enter to roll again or type \"Quit\" and then enter to quit."
+        );
+        rollAgain = !scanner.nextLine().matches("^\\s*[qQ](uit)?\\s*$");
+      }
     }
+  }
 }
